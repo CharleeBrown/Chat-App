5a5757da7ab92263e75a91b8e1fe00e6e9f3b0a5
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-05-11 15:26:12 -0500

    Add project files.

---
+++ SignalR_App/Hubs/talkHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+
+
+namespace SignalR_App.Hubs
+{
+    public class talkHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
